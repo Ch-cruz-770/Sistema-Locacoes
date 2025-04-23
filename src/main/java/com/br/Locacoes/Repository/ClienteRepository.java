d8b496244e9b682343d1779166b77dcb1f7a0b06
Cláudio Henrique <claudiohenriq017@gmail.com>
2025-04-22 23:27:47 -0300

    Delete src/main/java/com/br/Locacoes/Repository/ClienteRepository.java

--- src/main/java/com/br/Locacoes/Repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.Locacoes.Repository;
-
-import com.br.Locacoes.Model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
